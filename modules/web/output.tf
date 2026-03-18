aae52303a553b333221846d574ae16ed12a8adbe
Ayushi Vivek Dharkar <ayushidh@cybage.com>
2026-03-18 14:09:07 +0530

    reg script 11

--- modules/web/output.tf
+++ modules/web/output.tf
@@ -1,4 +1,7 @@
-output "alb_dns" { value = module.alb.dns_name }
+output "alb_dns" {
+  value = module.alb.lb_dns_name
+}
+
 output "target_group_arn" {
-  value = module.alb.target_groups["app"].arn
+  value = module.alb.target_group_arns[0]
 }
