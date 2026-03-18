426432f51dd06c1d7d0ad9dd5e06325082ed0c61
Ayushi Vivek Dharkar <ayushidh@cybage.com>
2026-03-18 11:23:48 +0530

    reg script 10

--- modules/web/main.tf
+++ modules/web/main.tf
@@ -1,10 +1,18 @@
 resource "aws_security_group" "alb_sg" {
+  name   = "${var.env}-alb-sg"
   vpc_id = var.vpc_id
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -15,29 +23,27 @@ module "alb" {
 
   name = "${var.env}-alb"
 
+  load_balancer_type = "application"
+
   vpc_id  = var.vpc_id
   subnets = var.subnets
 
   security_groups = [aws_security_group.alb_sg.id]
 
   target_groups = [
-  {
-    name_prefix      = "app"
-    backend_protocol = "HTTP"
-    backend_port     = 80
-    target_type      = "instance"
-  }
-]
+    {
+      name_prefix      = "app"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+    }
+  ]
 
-  listeners = [
-  {
-    port     = 80
-    protocol = "HTTP"
-
-    default_action = {
-      type             = "forward"
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
       target_group_index = 0
     }
-  }
-]
+  ]
 }
